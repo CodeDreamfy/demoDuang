b46300cbb8f56f8ad90c7a2e70f7dca48d16fea3
codedreamfy <codedreamfy@outlook.com>
2017-09-03 17:11:03 +0800

    up

--- slideUp/js/main.js
+++ slideUp/js/main.js
@@ -1,42 +1,53 @@
 $(function(){
-  console.log("sasasa");
   const elem = $('.wrap');
   let xStart, xEnd, xMt = 0;
   var maxHeight = window.innerHeight;
-  var gb = (420/maxHeight)*100;
+  var sectionHeight = $('section').outerHeight();
+  var minHeight = maxHeight-sectionHeight;
+  var placeDistance = 0;
+  var target = null
   elem.on('touchstart', function(e){
     xStart = e.changedTouches[0].pageY;
+    target = $(e.target).attr('class');
   })
   elem.on('touchmove', function(e){
     xEnd = e.changedTouches[0].pageY;
-    const cha = xStart-xEnd;
-    if(xEnd<xStart && cha>130 && xMt<=gb ) {
-      const h = 100 - cha/maxHeight*100;
-      $('.index').height(h+'%');
-      xMt = cha/maxHeight*100;
-    }else {
-
-    }
+    distanceIf(xEnd)
   })
   elem.on('touchend', function(e){
     xEnd = e.changedTouches[0].pageY;
-    const cha = xStart-xEnd;
-    if(xEnd<xStart && cha>130 && xMt<=gb ) {
-      const h = 100 - cha/maxHeight*100;
-      $('.index').height(gb+'%');
-      xMt = cha/maxHeight*100
-    }
+    distanceIf(xEnd,'end')
   })
 
-  function distanceIf (disY) {
+  function distanceIf (disY, tags) {
     xEnd = disY;
     const xdiff = xStart - xEnd;
-    if(xdiff>0){ // 向上滑
-      if(xMt<=gb){
-
+    if(xdiff>0){ // 向上滑 
+      if(placeDistance == minHeight) {
+        return false
       }
-    }else { // 向下滑
-
+      const ratio = (xdiff/sectionHeight)*100;
+      console.log(ratio)
+      if(tags){
+        if(ratio<50) {
+          placeDistance = 0;
+          $('.index').animate({height: '100%'}, '300ms');
+          return false;
+        }
+      }
+      if(ratio<36) {
+        placeDistance = ratio;
+        $('.index').height((100 - ratio)+'%');
+      }
+      else {
+        placeDistance = minHeight;
+        $('.index').animate({height: minHeight}, '300ms');
+      }
+    }else if(target && target == 'index' ||  placeDistance){
+      if(Math.abs(xdiff)>150) {
+        placeDistance = 0;
+        $('.index').animate({height: '100%'}, '300ms');
+      }
     }
   }
 })
