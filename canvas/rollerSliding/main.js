cfde3211d937f0240c5ca9a84d810fe0dae08ac9
codedreamfy <codedreamfy@Outlook.com>
2017-11-03 17:15:27 +0800

    插件进度10%

--- canvas/rollerSliding/main.js
+++ canvas/rollerSliding/main.js
@@ -1,4 +1,14 @@
-+function(window){
+(function(window){
+  var vw = document.body.clientWidth/100,
+    bodyFont = 100 / vw,
+    radiusBase = 7.12 * bodyFont * vw;
+  var argOptions = {
+    radius: radiusBase,
+    bgcolor: '#fcfcfc',
+    forecolor: 'rgba(95, 203, 253, 1)',
+    btnRadius:0.18  * bodyFont * vw,
+    lineWidth: 5
+  }
   var roller = function(canvasID, options) {
     return new roller.prototype.init(canvasID, options)
   }
@@ -16,12 +26,25 @@
         this.canvas = canvas;
       }
       if (typeof arguments[1] !== 'undefined') {
-        for( var key in options) {
+        for( var key in argOptions) {
+          if(key === 'radius' && options[key] ) {
+            options[key] = (options[key] / 100) * bodyFont * vw; // 将用户传递的值进行转换
+          }
           if( typeof options[key] == undefined) {
-            options[key2] = arg
+            options[key] = argOptions[key]
           }
         }
+        this.options = options;
+      }else {
+        this.options = argOptions;
       }
+      this.ctx = this.canvas.getContext('2d');
+      this.W = this.canvas.parentNode.offsetWidth,
+      this.H = this.canvas.parentNode.offsetHeight;
+      this.canvas.setAttribute('width', this.W );
+      this.canvas.setAttribute('height', this.H );
+
+
       return this;
     },
     bindEvent: function(){
@@ -32,4 +55,4 @@
   roller.prototype.init.prototype = roller.prototype;
   window.roller = roller;
   
-}(window, undefined);
+})(window, undefined);
